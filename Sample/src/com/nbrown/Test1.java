7ba076e1b621088584087947fa3781a8ff0b879d
pritam <pritam@DESKTOP-3PR64H6>
2018-01-21 13:56:59 +0530

    commit

---
+++ Sample/src/com/nbrown/Test1.java
@@ -0,0 +1,16 @@
+package com.nbrown;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+
+public class Test1 {
+
+	public static void main(String[] args) {
+
+		WebDriver driver = new FirefoxDriver();
+		driver.get("http://www.seleniumhq.org/download/");
+		
+		
+	}
+
+}
